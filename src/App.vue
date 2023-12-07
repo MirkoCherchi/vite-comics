8c4f1fbdc41293cbb5374c51c063c5e8c2ca19dc
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-12-07 15:41:21 +0100

    feat: scaffolding

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Header from './components/header/HeaderComponents.vue';
+export default {
+    name: 'App',
+    components: {
+        Header,
+
+    }
+
+}
+
+</script>
+
+<template>
+    <Header />
+</template>
+
+<style lang="scss"></style>
